bd2fc3d661967e8e061980f4fa3cf12d1d7d30cc
zh1peng <frueddy@gmail.com>
2025-04-27 11:21:46 +0800

    stimunit

--- src/run_trial.py
+++ src/run_trial.py
@@ -1,10 +1,10 @@
-from psyflow import TrialUnit
+from psyflow import StimUnit
 from functools import partial
 
 def run_trial(win, kb, settings, condition, stim_bank, trigger_sender, trigger_bank):
 
     trial_data = {"condition": condition}
-    make_unit = partial(TrialUnit, win=win, triggersender=trigger_sender)
+    make_unit = partial(StimUnit, win=win, triggersender=trigger_sender)
 
     make_unit(unit_label='movie').add_stim(stim_bank.get("movie")) \
         .show(duration=settings.movie_duration, 
